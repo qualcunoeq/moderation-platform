2fd99c6de0d980446333d73873c506252064bef4
qualcunoeq <francogigino73@gmail.com>
2025-06-30 16:31:24 +0200

    Convert next.config.ts to next.config.js and JS content

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Impostazioni di configurazione di Next.js
+    // Aggiungi qui qualsiasi configurazione specifica tu abbia,
+    // al momento non ne abbiamo aggiunte, quindi questo è sufficiente.
+};
+
+module.exports = nextConfig;
